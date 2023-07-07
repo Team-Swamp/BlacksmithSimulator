27b08d61c44352cded4a41d8bd5f2d355e7e9433
bas-boop <dereusbas461@gmail.com>
2023-07-07 22:03:46 +0200

    Added a way to drag mulitple items

--- Assets/Scripts/UI/DragAndDrop.cs
+++ Assets/Scripts/UI/DragAndDrop.cs
@@ -1,13 +1,25 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public sealed class DragAndDrop : MonoBehaviour
 {
-    private Vector3 _mousePos;
     [SerializeField] private GameObject currentItemToDrag;
-    [SerializeField, Tooltip("The height where the item is going to be dragged.")] private float dragHeight = 1;
+    
+    [Header("Settings")]
+    [SerializeField, Tooltip("The offset where the item is going to be dragged.")] private float dragOffset = 1;
+    [SerializeField] private Transform spawnPointForItems;
+
+    private Vector3 _mousePos;
+    private Collider _lastDraggedItemCollider;
+    
+    [Header("Events")]
+    
+    [SerializeField] private UnityEvent onSpawnItem = new UnityEvent();
 
     private void Update()
     {
+        SelectNewItemToDrag();
+        
         if (!Input.GetMouseButton(1)) return;
         
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -16,16 +28,34 @@ public sealed class DragAndDrop : MonoBehaviour
         {
             if(!hit.collider.gameObject.CompareTag("DragArea")) return;
             _mousePos = hit.point;
-            // Debug.Log("Mouse position in world space: " + _mousePos);
         }
-            
+
         if (!currentItemToDrag) return;
-        var itemPos = new Vector3(_mousePos.x, _mousePos.y, dragHeight);
+        
+        var itemPos = new Vector3(_mousePos.x, _mousePos.y, dragOffset);
         currentItemToDrag.transform.position = itemPos;
+        if (_lastDraggedItemCollider) _lastDraggedItemCollider.enabled = false;
+    }
+
+    private void SelectNewItemToDrag()
+    {
+        if (!Input.GetMouseButtonDown(1)) return;
+        
+        var ray1 = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray1, out var hit1))
+        {
+            if (!hit1.collider.gameObject.CompareTag("DraggableItem")) return;
+            if (_lastDraggedItemCollider) _lastDraggedItemCollider.enabled = true;
+            
+            currentItemToDrag = hit1.collider.gameObject;
+            _lastDraggedItemCollider = currentItemToDrag.GetComponent<Collider>();
+        }
     }
 
     public void SetItemToDrag(GameObject targetItem)
     {
-        currentItemToDrag = Instantiate(targetItem);
+        currentItemToDrag = Instantiate(targetItem, spawnPointForItems.position, targetItem.transform.rotation);
+        onSpawnItem?.Invoke();
     }
 }
