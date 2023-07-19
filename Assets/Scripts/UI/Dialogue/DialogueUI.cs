ca4895eda293295b82de8189b3860566c7ad7951
Tatum13 <tatumdv2001@gmail.com>
2023-07-19 15:57:24 +0200

    added progression to colored keywords
    Co-Authored-By: Bas de Reus <70578065+bas-boop@users.noreply.github.com>

--- Assets/Scripts/UI/Dialogue/DialogueUI.cs
+++ Assets/Scripts/UI/Dialogue/DialogueUI.cs
@@ -8,10 +8,12 @@ public sealed class DialogueUI : MonoBehaviour
     [SerializeField] private GameObject dialogueBox;
     [SerializeField] private TMP_Text textlabel;
     [SerializeField] private TMP_Text keywordText;
-    [SerializeField] private DialogueObject testDialogue;
+    [SerializeField] private DialogueObject startDialogue;
+    [SerializeField] private WeaponPartsDesirables weaponDesirables;
+    [SerializeField] private string keyWordColor;
 
     private int _dialogueIndex;
-    private string _randomDialogue;
+    private KeyWithText _randomDialogue;
     private TypeWriterEffect _typeWriterEffect;
 
     private void Start()
@@ -23,34 +25,34 @@ public sealed class DialogueUI : MonoBehaviour
     public void ShowDialogue(DialogueObject? dialogueObject)
     {
         dialogueBox.SetActive(true);
-        StartCoroutine(dialogueObject == null
-            ? StepThroughDialogue(testDialogue)
+        StartCoroutine(dialogueObject == null 
+            ? StepThroughDialogue(startDialogue) 
             : StepThroughDialogue(dialogueObject));
     }
 
     private IEnumerator StepThroughDialogue(DialogueObject dialogueObject)
     {
         _dialogueIndex = 1;
-        
+        Debug.Log(dialogueObject);
         var randomNumber = Random.Range(0, dialogueObject.TextWithKey.Count);
-        _randomDialogue = dialogueObject.TextWithKey[randomNumber].Dialogue[0];
+        _randomDialogue = dialogueObject.TextWithKey[randomNumber];
+        //_randomDialogue = dialogueObject.TextWithKey[randomNumber].Dialogue[0];
         
-        for (int i = 0; i < dialogueObject.TextWithKey[randomNumber].Dialogue.Length; i++)
-        {
-            yield return _typeWriterEffect.Run(_randomDialogue, textlabel);
-            yield return new WaitUntil(() => SetNextDialogue(dialogueObject.TextWithKey[randomNumber]));
-        }
-            
+        var dialogueCombinedWithColor = _randomDialogue.Dialogue[0] + keyWordColor + weaponDesirables.ToString() + "</color>" + _randomDialogue.Dialogue[1];
+
+        yield return _typeWriterEffect.Run(dialogueCombinedWithColor, textlabel);
+        yield return new WaitUntil(() => SetNextDialogue(dialogueObject.TextWithKey[randomNumber]));
+
         CloseDialogueBox();
     }
-
+    
      private bool SetNextDialogue(KeyWithText keyWithText)
      {
-         if (_dialogueIndex >= keyWithText.Dialogue.Length) return false;
+         //if (_dialogueIndex >= keyWithText.Dialogue.Length) return false;
          if (!Input.GetKeyDown(KeyCode.Space)) return false;
          
-         _randomDialogue = keyWithText.Dialogue[_dialogueIndex];
-         _dialogueIndex++;
+         //_randomDialogue = keyWithText.Dialogue[_dialogueIndex];
+         //_dialogueIndex++;
          return true;
      }
     
--- Assets/Scripts/UI/Dialogue/TypeWriterEffect.cs
+++ Assets/Scripts/UI/Dialogue/TypeWriterEffect.cs
@@ -21,9 +21,20 @@ public sealed class TypeWriterEffect : MonoBehaviour
         while (charIndex < textToType.Length)
         {
             theText += Time.deltaTime * typeWriterSpeed;
+            //if(the next charcter = <) search for the right arrow ">" if we have both of them, then type the arrow itself and the content in one go. after that go letter by letter.
             charIndex = Mathf.FloorToInt(theText);
             charIndex = Mathf.Clamp(charIndex, 0, textToType.Length);
-
+            if (textToType[charIndex] == '<')
+            {
+                Debug.Log(textToType[charIndex]);
+                for (int i = charIndex; i < textToType.Length; i++)
+                {
+                    if (textToType[i] == '>')
+                    {
+                        charIndex = i;
+                    }
+                }
+            }
             textLabel.text = textToType.Substring(0, charIndex);
             yield return null;
         }
